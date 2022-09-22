9196977fe7c5a90f395464267ba09e49173e5f91
JetteS <jette.steinbach@post.au.dk>
2022-09-22 10:11:28 +0200

    Add function hello_world

---
+++ R/Hello_world_function.R
@@ -0,0 +1,14 @@
+#' Hello World 
+#'
+#' @param my_name A string representing your name.
+#'
+#' @return
+#' The function prints a string
+#' @export
+#'
+#' @examples
+hello_world <- function(my_name = "James"){
+  
+  cat(paste0("Hello World! My name is ", as.character(my_name), " :) \n"))
+  cat(paste0("Hello", as.character(my_name), "! My name is ", randomNames::randomNames(n = 1, which.names = "both", name.order = "first.last", name.sep = " ")," :) \n"))
+}
